81dea9c8e3f52c100f48b283bad4ea1e53f72120
Dellarontay <dreadus@stanford.edu>
2020-12-18 11:50:18 -0800

    base

---
+++ webApp/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { LoginComponent } from './login/login.component';
+import { ChatRoomComponent } from './chat-room/chat-room.component';
+import { ContactsComponent } from './contacts/contacts.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingComponent,
+    LoginComponent,
+    ChatRoomComponent,
+    ContactsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
